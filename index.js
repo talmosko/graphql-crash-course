c384b9b15bcbc283e931e1e0e696826fb3dd284a
= <=>
2023-09-17 19:16:40 +0300

    yoga

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
-import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import express from "express";
+import { createSchema, createYoga } from "graphql-yoga";
 
 // data
-import db from './_db.js'
+import db from "./_db.js";
 
 // types
-import { typeDefs } from './schema.js'
+import { typeDefs } from "./schema.js";
 
 // resolvers
 const resolvers = {
   Query: {
     games() {
-      return db.games
+      return db.games;
     },
     game(_, args) {
-      return db.games.find((game) => game.id === args.id)
+      return db.games.find((game) => game.id === args.id);
     },
     authors() {
-      return db.authors
+      return db.authors;
     },
     author(_, args) {
-      return db.authors.find((author) => author.id === args.id)
+      return db.authors.find((author) => author.id === args.id);
     },
     reviews() {
-      return db.reviews
+      return db.reviews;
     },
     review(_, args) {
-      return db.reviews.find((review) => review.id === args.id)
-    }
+      return db.reviews.find((review) => review.id === args.id);
+    },
   },
   Game: {
     reviews(parent) {
-      return db.reviews.filter((r) => r.game_id === parent.id)
-    }
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
   },
   Review: {
     author(parent) {
-      return db.authors.find((a) => a.id === parent.author_id)
+      return db.authors.find((a) => a.id === parent.author_id);
     },
     game(parent) {
-      return db.games.find((g) => g.id === parent.game_id)
-    }
+      return db.games.find((g) => g.id === parent.game_id);
+    },
   },
   Author: {
     reviews(parent) {
-      return db.reviews.filter((r) => r.author_id === parent.id)
-    }
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
   },
   Mutation: {
     addGame(_, args) {
       let game = {
-        ...args.game, 
-        id: Math.floor(Math.random() * 10000).toString()
-      }
-      db.games.push(game)
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
 
-      return game
+      return game;
     },
     deleteGame(_, args) {
-      db.games = db.games.filter((g) => g.id !== args.id)
+      db.games = db.games.filter((g) => g.id !== args.id);
 
-      return db.games
+      return db.games;
     },
     updateGame(_, args) {
       db.games = db.games.map((g) => {
         if (g.id === args.id) {
-          return {...g, ...args.edits}
+          return { ...g, ...args.edits };
         }
 
-        return g
-      })
+        return g;
+      });
 
-      return db.games.find((g) => g.id === args.id)
-    }
-  }
-}
+      return db.games.find((g) => g.id === args.id);
+    },
+  },
+};
+const app = express();
+const schema = createSchema({ typeDefs, resolvers });
+const yoga = createYoga({ schema, graphiql: true });
+app.use(yoga.graphqlEndpoint, yoga);
 
-// server setup
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-})
-
-console.log(`Server ready at: ${url}`)
+app.listen(4000, () => {
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+});
